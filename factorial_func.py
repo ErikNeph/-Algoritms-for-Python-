2189d70356a2bff8f4848f5c6987dfa31f4fe583
Erik <128590288+ErikNeph@users.noreply.github.com>
2023-05-03 00:29:25 +0600

    Add files via upload

---
+++ factorial_func.py
@@ -0,0 +1,10 @@
+def fact(x):
+    if x == 1:
+        return 1
+    elif x == 0:
+        return 1
+    else:
+        return x * fact(x-1)
+
+
+print(fact(5)) # 5! = 5 * 4 * 3 * 2 * 1
---
+++ find_max.py
@@ -0,0 +1,12 @@
+def find_max(arr):
+    if len(arr) == 0:
+        return 0
+    elif len(arr) == 1:
+        return arr[0]
+    elif len(arr) == 2:
+        return arr[0] if arr[0] > arr[1] else arr[1]
+    sub_max = find_max(arr[1:])
+    return arr[0] if arr[0] > sub_max else sub_max
+
+
+print(find_max([11, 2, 54, 111, 3, 4, 45, 642, 1111, 34]))
